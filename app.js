8f20c933a81d5a6e98dc90a0fa2eb2d6d896e64c
Chung Nguyen <themask7786@gmail.com>
2022-01-25 14:55:37 +0700

    using passport, passport-local, passport-local-mongoose, express-session to authenticate

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -10,9 +13,21 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", `${__dirname}/views`);
 
-const Schema = mongoose.Schema;
-const userSchema = new Schema({
-  email: {
+//use session of module: express-session
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//use passport of module: passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
     require: true,
   },
@@ -22,10 +37,16 @@ const userSchema = new Schema({
   },
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+//use module: passport-local-mongoose to hash password (use with mongoose)
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+//use module: passport-local-mongoose to serialized and deserialize
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 mongoose
   .connect("mongodb://localhost:27017/secretsDB")
   .then(() => console.log("MongoDB connected."))
@@ -39,55 +60,53 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.json({
-      success: false,
-      message: "Missing email and/or password.",
-    });
-  }
-  try {
-    const foundUser = await User.findOne({ email });
-    if (foundUser) {
-      return res.json({ success: false, message: "This email already taken." });
-    }
-
-    const newUser = new User({ email, password });
-    await newUser.save();
-    res.render("secrets");
-  } catch {
-    (error) => console.error(error);
-    res.json({ succsess: false, message: "Internal server error." });
-  }
-});
-
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.json({
-      success: false,
-      message: "Missing email and/or password.",
-    });
-  }
-  try {
-    const foundUser = await User.findOne({ email });
-    if (!foundUser || foundUser.password !== password) {
-      return res.json({
-        success: false,
-        message: "Incorrect username and/or password.",
-      });
-    }
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
     res.render("secrets");
-  } catch (error) {
-    console.error(error);
-    res.json({ succsess: false, message: "Internal server error." });
+  } else {
+    res.redirect("/login");
   }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+  const { username, password } = req.body;
+  User.register({ username }, password, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    }
+
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/secrets");
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password,
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/login");
+    }
+
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/secrets");
+    });
+  });
 });
 
 const PORT = 3000;
